f9ecf02b3d5b7c8e8c507b338528dff9c031508d
st140715 <semonowmatwey@yandex.ru>
2025-09-25 20:24:40 +0300

    Update C:\Users\Master\Desktop\программы\2025_09_25.cpp

--- C:\Users\Master\Desktop\программы\2025_09_25.cpp
+++ C:\Users\Master\Desktop\программы\2025_09_25.cpp
@@ -1,1 +1,12 @@
+#include<cstdio>
 
+int main(int argc,char** argv)
+{
+    int a = 0;
+    int b = 0;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    int c = a + b;
+    printf("%d\n", c);
+    return 0;
+}
