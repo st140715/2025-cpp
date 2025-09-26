e6d923358e01ef51256d48e721ad2f2716c1b5ce
Matvey Master <semonowmatwey@yandex.ru>
2025-09-26 19:31:06 +0300

    initial commit

---
+++ ДЗ1/2025_09_25_4Zadan.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+
+int main(int argc,char** argv)
+{
+    int n = 0;
+    int a = 0;
+    int b = 0;
+    scanf("%d", &n);
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d\n", 2*a*b*n);
+    return 0;
+}
