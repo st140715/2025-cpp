e0ba621af4ae867a1acb77f783a279473fe032fe
Matvey Master <semonowmatwey@yandex.ru>
2025-10-06 20:35:24 +0300

    Передел 19Z

--- ДЗ1/2025_09_28_19Z.cpp
+++ ДЗ1/2025_09_28_19Z.cpp
@@ -4,12 +4,15 @@ int main(int argc,char** argv)
 {
     int sh = 0;
     scanf("%d", &sh);
+    int sign = (sh > 0) - (sh < 0);
     int ia = 0;
     scanf("%d", &ia);
     printf("%d", ia/sh);
     printf(" ");
-    printf("%d", ia - sh*(ia/sh) );
+    int k = ia - sh*(ia/sh);
+    int sign = (k > 0) or (k < 0);
+    printf("%d", k );
     printf(" ");
-    printf("%d", (sh - ( ia - sh*(ia/sh) ) )*(ia - sh*(ia/sh)!=0) );
+    printf("%d", (sh - a)*sign );
     return 0;
 }
