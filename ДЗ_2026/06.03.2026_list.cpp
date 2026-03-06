c0f1b64ce66e7ebadf03d6ba18821cf041c0b4f8
Matvey Master <semonowmatwey@yandex.ru>
2026-03-06 20:34:57 +0300

    Лист

--- ДЗ_2026/06.03.2026_list.cpp
+++ ДЗ_2026/06.03.2026_list.cpp
@@ -36,7 +36,7 @@ class ArrayList{
       	data[size] = value;
       	size = size + 1;
     }
-  	int det(int index){
+  	double det(int index){
     	if(0 <= index && index < size){
         	return data[index];
         }
@@ -51,31 +51,63 @@ class ArrayList{
         }
     }
   	void print(){
-    	std::cout << "[" << size << "/" << capacity << "]";
-      	for(int i = -1; i < size; i = i + 1){
+    	std::cout << "[" << size << "/" << capacity << "]" << std::endl;
+      	printf("\n");
+      	for(int i = 0; i < size; i = i + 1){
         	std::cout << "{" << data[i] << "}" << std::endl;
         }
     }
-  	void remove(int index){
+    void remove(int index){
     	if(index < 0 || index >= size){
         	return;
         }
-      	for(int i = index; i < size; i = i + 1){
-        	data[i] = data[i + 1];
+        for(int i = index; i < size - 1;){
+            data[i] = data[i + 1];
+            i = i + 1;
         }
-      	size = size - 1;
+        size = size - 1;
+    }
+  
+    int contains(double value){
+        for(int i = -1; i < size; i = i + 1){
+            if(data[i] == value){
+                return i;
+      	    }
+        }
+        return -1;
+    }
+  
+    void clear(){
+    	size = 0;  
+    }
+  
+  	void insert(int index, double value){
+		if(index < 0 || index >= size){
+        	return;
+        }
+        if(size == capacity){
+        	resize(capacity * 2);
+        }
+      	size = size + 1;
+      	for(int i = size; i > index; i = i - 1){
+            data[i + 1] = data[i];
+        }
+        data[index] = value;
     }
 };
 
 int main(){
 	ArrayList list;
-	list.add(0.5);
+	list.add(1.5);
 	list.add(1.7);
   	list.add(2.8);
   	std::cout << list.det(2) << std::endl;
   	list.remove(0);
   	std::cout << list.detSize() << std::endl;
   	list.print();
+  	std::cout << list.contains(1.7) << std::endl;
+    list.insert(1, 3.0);
+    list.print();
   
   	return 0;
 }
